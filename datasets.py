470a86901d2115fbbd4838286d521eb77072ce83
ADG-max <150793755+ADG-max@users.noreply.github.com>
2025-12-02 18:31:21 +0700

    datasets.py

--- datasets.py
+++ datasets.py
@@ -8,6 +8,7 @@ import random
 from xml.etree import ElementTree as et
 from torch.utils.data import Dataset, DataLoader
 from utils.transforms import (
+    apply_small_object_aug,
     get_train_transform, 
     get_valid_transform,
     get_train_aug,
@@ -438,6 +439,18 @@ class CustomDataset(Dataset):
                     index=idx
                 )
 
+            image_resized_np = (image_resized * 255).astype(np.uint8)
+            boxes_list = boxes.cpu().numpy().tolist()
+            labels_list = labels.cpu().numpy().tolist()
+        
+            image_resized_np, boxes_list, labels_list = apply_small_object_aug(
+                image_resized_np, boxes_list, labels_list
+            )
+        
+            image_resized = image_resized_np.astype(np.float32) / 255.0
+            boxes = torch.tensor(boxes_list, dtype=torch.float32)
+            labels = torch.tensor(labels_list, dtype=torch.int64)
+        
         # Prepare the final `target` dictionary.
         target = {}
         target["boxes"] = boxes
@@ -451,24 +464,19 @@ class CustomDataset(Dataset):
         labels = labels.cpu().numpy().tolist()  # Convert tensor to list
         bboxes = target['boxes'].cpu().numpy().tolist()
 
-        # Tambahkan augmentasi khusus objek kecil
-        image_resized, bboxes, labels = apply_small_object_aug(
-            image_resized, bboxes, labels
-        )
-
         if self.use_train_aug: # Use train augmentation if argument is passed.
             train_aug = get_train_aug()
             sample = train_aug(image=image_resized,
                                      bboxes=bboxes,
                                      labels=labels)
             image_resized = sample['image']
-            target['boxes'] = torch.Tensor(sample['bboxes']).to(torch.int64)
+            target['boxes'] = torch.tensor(sample['bboxes'], dtype=torch.float32)
         else:
             sample = self.transforms(image=image_resized,
                                      bboxes=bboxes,
                                      labels=labels)
             image_resized = sample['image']
-            target['boxes'] = torch.Tensor(sample['bboxes']).to(torch.int64)
+            target['boxes'] = torch.tensor(sample['bboxes'], dtype=torch.float32)
 
         # Fix to enable training without target bounding boxes,
         # see https://discuss.pytorch.org/t/fasterrcnn-images-with-no-objects-present-cause-an-error/117974/4
