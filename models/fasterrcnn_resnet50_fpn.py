625a7a11cbf16658cd0359ead48fef20c517dd21
ADG-max <150793755+ADG-max@users.noreply.github.com>
2025-12-07 01:02:16 +0700

    fasterrcnn_resnet50_fpn.py

--- models/fasterrcnn_resnet50_fpn.py
+++ models/fasterrcnn_resnet50_fpn.py
@@ -5,7 +5,7 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 def create_model(num_classes, pretrained=True, coco_model=False):
     # Load Faster RCNN pre-trained model
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
-        weights='DEFAULT'
+        weights=torchvision.models.detection.FasterRCNN_ResNet50_FPN_Weights.DEFAULT
     )
     if coco_model: # Return the COCO pretrained model for COCO classes.
         return model, coco_model
